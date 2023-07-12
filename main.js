1dfb5623c39838f0273947ac13d87806645d68e8
Rintaro Fujita <72987720+RintaroFujita@users.noreply.github.com>
2023-07-12 16:15:25 +0900

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-	const loginButton = document.querySelector(".LoginButton");
-	const dropdownMenu = document.querySelector(".dropdown__menu");
-    
-	loginButton.addEventListener("click", function() {
-	  dropdownMenu.hidden = !dropdownMenu.hidden;
-	});
-    });
-    
